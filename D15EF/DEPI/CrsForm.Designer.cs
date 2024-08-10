653aeab87fcda45d3ab72dd6924d4027bc932db8
Mostafa Bkry <mbkry661@gmail.com>
2024-08-10 08:01:33 +0300

    Finished Course Form

--- D15EF/DEPI/CrsForm.Designer.cs
+++ D15EF/DEPI/CrsForm.Designer.cs
@@ -30,6 +30,21 @@
         {
             BackButton = new Button();
             StdGView = new DataGridView();
+            label5 = new Label();
+            TopicTBox = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            DurationTBox = new TextBox();
+            NameTBox = new TextBox();
+            IdTBox = new TextBox();
+            ShowStudButton = new Button();
+            ShowInstsButton = new Button();
+            ShowTopicButton = new Button();
+            RestButton = new Button();
+            DistButton = new Button();
+            LstButton = new Button();
+            FstButton = new Button();
             ((System.ComponentModel.ISupportInitialize)StdGView).BeginInit();
             SuspendLayout();
             // 
@@ -50,18 +65,191 @@
             // 
             StdGView.BackgroundColor = SystemColors.ControlLight;
             StdGView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            StdGView.Location = new Point(126, 206);
+            StdGView.Location = new Point(33, 287);
             StdGView.Name = "StdGView";
-            StdGView.Size = new Size(545, 240);
+            StdGView.Size = new Size(732, 240);
             StdGView.TabIndex = 2;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            label5.ForeColor = SystemColors.ButtonFace;
+            label5.Location = new Point(367, 63);
+            label5.Name = "label5";
+            label5.Size = new Size(66, 20);
+            label5.TabIndex = 67;
+            label5.Text = "Topic ID";
+            // 
+            // TopicTBox
+            // 
+            TopicTBox.Location = new Point(458, 65);
+            TopicTBox.Name = "TopicTBox";
+            TopicTBox.Size = new Size(125, 23);
+            TopicTBox.TabIndex = 65;
+            TopicTBox.TextChanged += TopicTBox_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            label3.ForeColor = SystemColors.ButtonFace;
+            label3.Location = new Point(367, 22);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 20);
+            label3.TabIndex = 63;
+            label3.Text = "Duration";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            label2.ForeColor = SystemColors.ButtonFace;
+            label2.Location = new Point(104, 64);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 20);
+            label2.TabIndex = 64;
+            label2.Text = "Name";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(116, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(25, 20);
+            label1.TabIndex = 62;
+            label1.Text = "ID";
+            // 
+            // DurationTBox
+            // 
+            DurationTBox.Location = new Point(458, 22);
+            DurationTBox.Name = "DurationTBox";
+            DurationTBox.Size = new Size(125, 23);
+            DurationTBox.TabIndex = 61;
+            DurationTBox.TextChanged += DurationTBox_TextChanged;
+            // 
+            // NameTBox
+            // 
+            NameTBox.Location = new Point(171, 64);
+            NameTBox.Name = "NameTBox";
+            NameTBox.Size = new Size(125, 23);
+            NameTBox.TabIndex = 60;
+            NameTBox.TextChanged += NameTBox_TextChanged;
+            // 
+            // IdTBox
+            // 
+            IdTBox.Location = new Point(171, 22);
+            IdTBox.Name = "IdTBox";
+            IdTBox.Size = new Size(125, 23);
+            IdTBox.TabIndex = 59;
+            IdTBox.TextChanged += IdTBox_TextChanged;
+            // 
+            // ShowStudButton
+            // 
+            ShowStudButton.Font = new Font("Segoe UI", 10F);
+            ShowStudButton.Location = new Point(475, 190);
+            ShowStudButton.Name = "ShowStudButton";
+            ShowStudButton.Size = new Size(198, 30);
+            ShowStudButton.TabIndex = 57;
+            ShowStudButton.Text = "Show Course Students";
+            ShowStudButton.UseVisualStyleBackColor = true;
+            ShowStudButton.Click += ShowStudButton_Click;
+            // 
+            // ShowInstsButton
+            // 
+            ShowInstsButton.Font = new Font("Segoe UI", 10F);
+            ShowInstsButton.Location = new Point(248, 190);
+            ShowInstsButton.Name = "ShowInstsButton";
+            ShowInstsButton.Size = new Size(198, 30);
+            ShowInstsButton.TabIndex = 56;
+            ShowInstsButton.Text = "Show Course Instructors";
+            ShowInstsButton.UseVisualStyleBackColor = true;
+            ShowInstsButton.Click += ShowInstsButton_Click;
+            // 
+            // ShowTopicButton
+            // 
+            ShowTopicButton.Font = new Font("Segoe UI", 10F);
+            ShowTopicButton.Location = new Point(33, 190);
+            ShowTopicButton.Name = "ShowTopicButton";
+            ShowTopicButton.Size = new Size(185, 30);
+            ShowTopicButton.TabIndex = 55;
+            ShowTopicButton.Text = "Show Courses Topic";
+            ShowTopicButton.UseVisualStyleBackColor = true;
+            ShowTopicButton.Click += ShowManagedButton_Click;
+            // 
+            // RestButton
+            // 
+            RestButton.Font = new Font("Segoe UI", 9F);
+            RestButton.Location = new Point(678, 236);
+            RestButton.Name = "RestButton";
+            RestButton.Size = new Size(87, 33);
+            RestButton.TabIndex = 54;
+            RestButton.Text = "Reset";
+            RestButton.UseVisualStyleBackColor = true;
+            RestButton.Click += RestButton_Click;
+            // 
+            // DistButton
+            // 
+            DistButton.Font = new Font("Segoe UI", 9F);
+            DistButton.Location = new Point(260, 236);
+            DistButton.Name = "DistButton";
+            DistButton.Size = new Size(87, 33);
+            DistButton.TabIndex = 53;
+            DistButton.Text = "Distinct Crs";
+            DistButton.UseVisualStyleBackColor = true;
+            DistButton.Click += DistButton_Click;
+            // 
+            // LstButton
+            // 
+            LstButton.Font = new Font("Segoe UI", 9F);
+            LstButton.Location = new Point(147, 236);
+            LstButton.Name = "LstButton";
+            LstButton.Size = new Size(87, 33);
+            LstButton.TabIndex = 58;
+            LstButton.Text = "Last Course";
+            LstButton.UseVisualStyleBackColor = true;
+            LstButton.Click += LstButton_Click;
+            // 
+            // FstButton
+            // 
+            FstButton.Font = new Font("Segoe UI", 9F);
+            FstButton.Location = new Point(33, 236);
+            FstButton.Name = "FstButton";
+            FstButton.Size = new Size(87, 33);
+            FstButton.TabIndex = 52;
+            FstButton.Text = "First Course";
+            FstButton.UseVisualStyleBackColor = true;
+            FstButton.Click += FstButton_Click;
             // 
             // CrsForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.BackG2;
-            ClientSize = new Size(800, 450);
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 530);
             ControlBox = false;
+            Controls.Add(label5);
+            Controls.Add(TopicTBox);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(DurationTBox);
+            Controls.Add(NameTBox);
+            Controls.Add(IdTBox);
+            Controls.Add(ShowStudButton);
+            Controls.Add(ShowInstsButton);
+            Controls.Add(ShowTopicButton);
+            Controls.Add(RestButton);
+            Controls.Add(DistButton);
+            Controls.Add(LstButton);
+            Controls.Add(FstButton);
             Controls.Add(StdGView);
             Controls.Add(BackButton);
             Name = "CrsForm";
@@ -69,11 +257,27 @@
             Text = "CrsForm";
             ((System.ComponentModel.ISupportInitialize)StdGView).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button BackButton;
         private DataGridView StdGView;
+        private Label label5;
+        private TextBox TopicTBox;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox DurationTBox;
+        private TextBox NameTBox;
+        private TextBox IdTBox;
+        private Button ShowStudButton;
+        private Button ShowInstsButton;
+        private Button ShowTopicButton;
+        private Button RestButton;
+        private Button DistButton;
+        private Button LstButton;
+        private Button FstButton;
     }
 }
--- D15EF/DEPI/CrsForm.cs
+++ D15EF/DEPI/CrsForm.cs
@@ -1,20 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using DBModels;
 
 namespace DEPI
 {
     public partial class CrsForm : Form
     {
+        DEPIContext st = new DEPIContext();
+        List<Course> courses;
+
         public CrsForm()
         {
             InitializeComponent();
+            courses = st.Courses.ToList();
+
+            StdGView.DataSource = courses.Select(c => new
+            { c.Crs_Id, c.Crs_Name, c.Crs_Duration, c.Topic_Id }).ToList();
+        }
+
+
+        #region Buttons Logic
+        public List<Course> CheckMethodForButtons()
+        {
+            List<Course> stLst = new List<Course>();
+            int id; int? topic_Id, cDuration;
+            string cName;
+
+            for (int row = 0; row < StdGView?.Rows.Count; row++)
+            {
+                id =
+                    int.TryParse(StdGView.Rows[row].Cells["Crs_Id"].Value.ToString(), out int chkId) ?
+                    chkId : -1;
+
+                cName = StdGView.Rows[row].Cells["Crs_Name"].Value.ToString() ?? "NA";
+
+                topic_Id =
+                    int.TryParse(StdGView?.Rows[row]?.Cells["Topic_Id"]?.Value?.ToString(), out int chkTopId) ?
+                    chkTopId : null;
+
+                cDuration =
+                    int.TryParse(StdGView?.Rows[row]?.Cells["Crs_Duration"]?.Value?.ToString(), out int chkDuration) ?
+                    chkDuration : -1;
+
+
+                stLst.Add(new Course()
+                {
+                    Crs_Id = id,
+                    Crs_Name = cName,
+                    Topic_Id = topic_Id,
+                    Crs_Duration = cDuration
+                });
+            }
+
+            return stLst;
         }
 
         private void BackButton_Click(object sender, EventArgs e)
@@ -23,5 +59,154 @@ namespace DEPI
             if (Tag is FirstPageForm f1)
                 f1.Show();
         }
+
+        private void RestButton_Click(object sender, EventArgs e)
+        {
+            IdTBox.Clear();
+            NameTBox.Clear();
+            DurationTBox.Clear();
+            TopicTBox.Clear();
+
+            StdGView.DataSource = courses
+                .Select(c => new
+                { c.Crs_Id, c.Crs_Name, c.Crs_Duration, c.Topic_Id })
+                .ToList();
+        }
+
+        private void FstButton_Click(object sender, EventArgs e)
+        {
+            var result = new List<Course>();
+
+            try
+            {
+                result.Add(CheckMethodForButtons()?.FirstOrDefault() ?? new Course());
+                StdGView.DataSource = result.Select(c => new
+                { c.Crs_Id, c.Crs_Name, c.Crs_Duration, c.Topic_Id })
+                .ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+            }
+        }
+
+        private void LstButton_Click(object sender, EventArgs e)
+        {
+            var result = new List<Course?>();
+
+            try
+            {
+                result.Add(CheckMethodForButtons()?.LastOrDefault() ?? new Course());
+                StdGView.DataSource = result.Select(c => new
+                { c.Crs_Id, c.Crs_Name, c.Crs_Duration, c.Topic_Id })
+                .ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+            }
+        }
+
+        private void DistButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var r = CheckMethodForButtons()
+                   .DistinctBy(c => c.Crs_Name)
+                   .Select(c => new
+                   { c.Crs_Id, c.Crs_Name, c.Crs_Duration, c.Topic_Id })
+                   .ToList();
+
+                StdGView.DataSource = r;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void ShowManagedButton_Click(object sender, EventArgs e)
+        {
+            StdGView.DataSource = courses.Join(st.Topics, c => c.Topic_Id, t => t.Top_Id, (c, t)
+                => new
+                { c.Crs_Id, c.Crs_Name, t.Top_Id, t.Topic_Name }).ToList();
+        }
+
+        private void ShowInstsButton_Click(object sender, EventArgs e)
+        {
+            var instCrs = courses.Join(st.InstCourses, c => c.Crs_Id, ic => ic.Crs_Id,
+                (c, ic) => new { c.Crs_Id, c.Crs_Name, ic.Ins_Id, ic.Evaluation }).ToList();
+            var inst = instCrs.Join(st.Instructors, ic => ic.Ins_Id, i => i.Ins_Id,
+                (ic, i) => new { ic.Crs_Id, ic.Crs_Name, ic.Ins_Id, i.Ins_Name, ic.Evaluation });
+
+            var r = inst.ToList();
+
+            StdGView.DataSource = r;
+        }
+
+        private void ShowStudButton_Click(object sender, EventArgs e)
+        {
+            var studCrs = courses.Join(st.StudCourses, c => c.Crs_Id, sc => sc.Crs_Id,
+                (c, sc) => new { c.Crs_Id, c.Crs_Name, sc.Stud_Id, sc.Grade }).ToList();
+            var stud = studCrs.Join(st.Students, sc => sc.Stud_Id, s => s.St_Id,
+                (sc, s) => new { sc.Crs_Id, sc.Crs_Name, sc.Stud_Id, s.Fname, s.Sname, sc.Grade });
+
+            var r = stud.ToList();
+
+            StdGView.DataSource = r;
+        }
+        #endregion
+
+        #region TextBoxes Logic
+        private void IdTBox_TextChanged(object sender, EventArgs e)
+        {
+            var r = courses.Where(c =>
+                c.Crs_Id == (int.TryParse(IdTBox.Text, out int check) ? check : -1))
+                .Select(c => new
+                { c.Crs_Id, c.Crs_Name, c.Crs_Duration, c.Topic_Id })
+                .ToList();
+
+            StdGView.DataSource = string.IsNullOrEmpty(IdTBox.Text) ? courses.Select(c => new
+            { c.Crs_Id, c.Crs_Name, c.Crs_Duration, c.Topic_Id }).ToList() : r;
+        }
+
+        private void NameTBox_TextChanged(object sender, EventArgs e)
+        {
+            var r = courses.Where(c =>
+                c.Crs_Name?.Contains(NameTBox.Text) ?? false)
+                .Select(c => new
+                { c.Crs_Id, c.Crs_Name, c.Crs_Duration, c.Topic_Id })
+                .ToList();
+
+            StdGView.DataSource = string.IsNullOrEmpty(NameTBox.Text) ? courses.Select(c => new
+            { c.Crs_Id, c.Crs_Name, c.Crs_Duration, c.Topic_Id }).ToList() : r;
+        }
+
+        private void DurationTBox_TextChanged(object sender, EventArgs e)
+        {
+            var r = courses.Where(c =>
+                c.Crs_Duration == (int.TryParse(DurationTBox.Text, out int check) ? check : -1))
+                .Select(c => new
+                { c.Crs_Id, c.Crs_Name, c.Crs_Duration, c.Topic_Id })
+                .ToList();
+
+            StdGView.DataSource = string.IsNullOrEmpty(DurationTBox.Text) ? courses.Select(c => new
+            { c.Crs_Id, c.Crs_Name, c.Crs_Duration, c.Topic_Id }).ToList() : r;
+        }
+
+        private void TopicTBox_TextChanged(object sender, EventArgs e)
+        {
+            var r = courses.Where(c =>
+                c.Topic_Id == (int.TryParse(TopicTBox.Text, out int check) ? check : -1))
+                .Select(c => new
+                { c.Crs_Id, c.Crs_Name, c.Crs_Duration, c.Topic_Id })
+                .ToList();
+
+            StdGView.DataSource = string.IsNullOrEmpty(TopicTBox.Text) ? courses.Select(c => new
+            { c.Crs_Id, c.Crs_Name, c.Crs_Duration, c.Topic_Id }).ToList() : r;
+        }
+        #endregion
     }
 }
